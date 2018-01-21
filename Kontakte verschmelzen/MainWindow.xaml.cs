b761cf3b6617a060525d7f19b41d995121730dc7
David Nelles <david.nelles@dlr.de>
2018-01-21 21:42:32 +0100

    Bild wird nun vollstaendig eingelsen

--- Kontakte verschmelzen/MainWindow.xaml.cs
+++ Kontakte verschmelzen/MainWindow.xaml.cs
@@ -30,14 +30,14 @@ namespace Kontakte_verschmelzen
 
         private void btn_Start_Click(object sender, RoutedEventArgs e)
         {
-            Datei_Einlesen(@"E:\Eingang\dr.alles.vcf");
+            Datei_Einlesen(@"E:\Eingang\drmi.vcf");
         }
 
         private void Datei_Einlesen(string path)
         {
             System.IO.StreamReader file = new StreamReader(path);
 
-            string line;
+            string line = "", Typbefore = "";
             CKontakt Kontakt = new CKontakt();
 
             while ((line = file.ReadLine()) != null)
@@ -57,170 +57,201 @@ namespace Kontakte_verschmelzen
                     string Untertyp = "";
                     string Wert = "";
 
-                    while (line[i] != ':')
+                    while (i < line.Length && line[i] != ':')
                     {
                         Typ += line[i];
                         i++;
                     }
 
-                    i++;
-
-                    Wert = line.Substring(i);
-
-                    if (GetCountofChar(';', Typ) >= 1)
+                    if(i >= line.Length)
                     {
-                        i = 0;
-                        while (Typ[i] != ';')
-                            i++;
+                        Typ = "";
+                    }
+                    else
+                    {
+                        i++;
 
-                        Untertyp = Typ.Substring(i + 1);
-                        Typ = Typ.Substring(0, i);
+                        Wert = line.Substring(i);
+
+                        if (GetCountofChar(';', Typ) >= 1)
+                        {
+                            i = 0;
+                            while (Typ[i] != ';')
+                                i++;
+
+                            Untertyp = Typ.Substring(i + 1);
+                            Typ = Typ.Substring(0, i);
+                        }
                     }
 
-                    switch (Typ)
+                    if (Typ != "")
                     {
-                        #region Version
-                        case "VERSION":
-                            Kontakt.version = Wert;
-                            break;
-                        #endregion
-
-                        #region Name
-                        case "N":
-                            i = 0;
-                            while (Wert[i] != ';')
-                            {
-                                Kontakt.Name.NachName += Wert[i];
-                                i++;
-                            }
-
-                            i++;
-
-                            while (Wert[i] != ';')
-                            {
-                                Kontakt.Name.VorName += Wert[i];
-                                i++;
-                            }
-
-                            i++;
-
-                            while (Wert[i] != ';')
-                            {
-                                Kontakt.Name.ZweitName += Wert[i];
-                                i++;
-                            }
-
-                            i++;
-
-                            while (Wert[i] != ';')
-                            {
-                                Kontakt.Name.Praefix += Wert[i];
-                                i++;
-                            }
-
-                            i++;
-
-                            Kontakt.Name.Suffix += Wert.Substring(i);
-                            break;
-                        #endregion
-
-                        #region FN
-                        case "FN":
-                            Kontakt.FullName = Wert;
-                            break;
-                        #endregion
-
-                        #region Phonetic Name
-                        #region case X-PHONETIC-FIRST-NAME
-                        case "X-PHONETIC-FIRST-NAME":
-                            Kontakt.PhoneticName.FirstName = Wert;
-                            break;
-                        #endregion
-
-                        #region case X-PHONETIC-MIDDLE-NAME
-                        case "X-PHONETIC-MIDDLE-NAME":
-                            Kontakt.PhoneticName.MiddleName = Wert;
-                            break;
-                        #endregion
-
-                        #region case X-PHONETIC-LAST-NAME
-                        case "X-PHONETIC-LAST-NAME":
-                            Kontakt.PhoneticName.LastName = Wert;
-                            break;
-                        #endregion
-                        #endregion
-
-                        #region Telefon
-                        case "TEL":
-                            Kontakt.TelNumber.Add(new Itm(Untertyp, Wert));
-                            break;
-                        #endregion
-
-                        #region EMail
-                        case "EMAIL":
-                            Kontakt.EMail.Add(new Itm(Untertyp, Wert));
-                            break;
-                        #endregion
-
-                        #region Adresse
-                        case "ADR":
-                            Kontakt.Adr.Add(new Itm(Untertyp, Wert));
-                            break;
-                        #endregion
-
-                        #region Titel
-                        case "TITLE":
-                            Kontakt.Titel = Wert;
-                            break;
-                        #endregion
-
-                        #region Organisation
-                        case "ORG":
-                            Kontakt.Organisation = Wert;
-                            break;
-                        #endregion
-
-                        #region Websites
-                        case "URL":
-                            Kontakt.Website.Add(Wert);
-                            break;
-                        #endregion
-
-                        #region Photo  
-                        case "PHOTO":
-                            Kontakt.img = Wert;
-                            break;
-                        #endregion
-
-                        #region Note  
-                        case "NOTE":
-                            Kontakt.Note = Wert;
-                            break;
-                        #endregion
-
-                        #region Birthday  
-                        case "BDAY":
-                            Kontakt.Birthday = Wert;
-                            break;
-                        #endregion
-
-                        #region Nickname & Event
-                        case "X-ANDROID-CUSTOM":
-                            if (Wert.Substring(23, 9) == "/nickname")
-                                Kontakt.nickname = Wert;
-                            else
-                                Kontakt.ev.Add(Wert);
-                            break;
-                        #endregion
-
-                        #region Rest Meist Messenger
-                        default:
-                            Kontakt.MessegersAndOthers.Add(new Itm(Typ, Wert));
-                            break;
+                        switch (Typ)
+                        {
+                            #region Version
+                            case "VERSION":
+                                Kontakt.version = Wert;
+                                break;
                             #endregion
+
+                            #region Name
+                            case "N":
+                                i = 0;
+                                while (Wert[i] != ';')
+                                {
+                                    Kontakt.Name.NachName += Wert[i];
+                                    i++;
+                                }
+
+                                i++;
+
+                                while (Wert[i] != ';')
+                                {
+                                    Kontakt.Name.VorName += Wert[i];
+                                    i++;
+                                }
+
+                                i++;
+
+                                while (Wert[i] != ';')
+                                {
+                                    Kontakt.Name.ZweitName += Wert[i];
+                                    i++;
+                                }
+
+                                i++;
+
+                                while (Wert[i] != ';')
+                                {
+                                    Kontakt.Name.Praefix += Wert[i];
+                                    i++;
+                                }
+
+                                i++;
+
+                                Kontakt.Name.Suffix += Wert.Substring(i);
+                                break;
+                            #endregion
+
+                            #region FN
+                            case "FN":
+                                Kontakt.FullName = Wert;
+                                break;
+                            #endregion
+
+                            #region Phonetic Name
+                            #region case X-PHONETIC-FIRST-NAME
+                            case "X-PHONETIC-FIRST-NAME":
+                                Kontakt.PhoneticName.FirstName = Wert;
+                                break;
+                            #endregion
+
+                            #region case X-PHONETIC-MIDDLE-NAME
+                            case "X-PHONETIC-MIDDLE-NAME":
+                                Kontakt.PhoneticName.MiddleName = Wert;
+                                break;
+                            #endregion
+
+                            #region case X-PHONETIC-LAST-NAME
+                            case "X-PHONETIC-LAST-NAME":
+                                Kontakt.PhoneticName.LastName = Wert;
+                                break;
+                            #endregion
+                            #endregion
+
+                            #region Telefon
+                            case "TEL":
+                                Kontakt.TelNumber.Add(new Itm(Untertyp, Wert));
+                                break;
+                            #endregion
+
+                            #region EMail
+                            case "EMAIL":
+                                Kontakt.EMail.Add(new Itm(Untertyp, Wert));
+                                break;
+                            #endregion
+
+                            #region Adresse
+                            case "ADR":
+                                Kontakt.Adr.Add(new Itm(Untertyp, Wert));
+                                break;
+                            #endregion
+
+                            #region Titel
+                            case "TITLE":
+                                Kontakt.Titel = Wert;
+                                break;
+                            #endregion
+
+                            #region Organisation
+                            case "ORG":
+                                Kontakt.Organisation = Wert;
+                                break;
+                            #endregion
+
+                            #region Websites
+                            case "URL":
+                                Kontakt.Website.Add(Wert);
+                                break;
+                            #endregion
+
+                            #region Photo  
+                            case "PHOTO":
+                                Kontakt.img = Wert;
+                                break;
+                            #endregion
+
+                            #region Note  
+                            case "NOTE":
+                                Kontakt.Note = Wert;
+                                break;
+                            #endregion
+
+                            #region Birthday  
+                            case "BDAY":
+                                Kontakt.Birthday = Wert;
+                                break;
+                            #endregion
+
+                            #region Nickname & Event
+                            case "X-ANDROID-CUSTOM":
+                                if (Wert.Substring(23, 9) == "/nickname")
+                                    Kontakt.nickname = Wert;
+                                else
+                                    Kontakt.ev.Add(Wert);
+                                break;
+                            #endregion
+
+                            #region Rest Meist Messenger
+                            default:
+                                Kontakt.MessegersAndOthers.Add(new Itm(Typ, Wert));
+                                break;
+                            #endregion
+                        }
+                        Typbefore = Typ;
+                    }
+                    else if(Typbefore == "PHOTO")
+                    {
+                        Kontakt.img += CleanString(line);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid Inputfile", "Error:", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                     }
                 }
             }
+        }
+
+        private string CleanString(string value)
+        {
+            for (int i = 0;  i < value.Length; i++)
+            {
+                if (value[i] == ' ' && (i + 1 == value.Length || value[i + 1] == ' ' || i - 1 < 0))
+                {
+                    value = value.Substring(0, i) + value.Substring(i + 1);
+                }
+            }
+            return value;
         }
 
         private int GetCountofChar(char Char, string String)
